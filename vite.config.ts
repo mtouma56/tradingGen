10010e224469389677109c36cc67b17a67423e43
mtouma56 <mtouma56@gmail.com>
2025-08-27 17:03:20 -0400

    feat: allow dynamic host access

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     strictPort: true,
-    allowedHosts: [
-      '3000-icmsarcraz9y3bljxdtx8-6532622b.e2b.dev',
-      'localhost',
-      '127.0.0.1',
-      '0.0.0.0'
-    ],
+    allowedHosts: process.env.HOST ? [process.env.HOST] : true,
     cors: true,
     hmr: {
       clientPort: 3000
